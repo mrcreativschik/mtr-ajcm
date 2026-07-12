401a93f7ed0c8b2bdf352008842e9966887f7622
Mrcreativschik <krukovilya04@gmail.com>
2026-07-13 01:00:59 +0300

    Removed sowcer (became obsolete)

--- _obsolete/sowcerext/util/Logging.java
+++
@@ -1,9 +0,0 @@
-package cn.zbx1425.sowcerext.util;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class Logging {
-
-    public static final Logger LOGGER = LogManager.getLogger("SowCerExt");
-}
